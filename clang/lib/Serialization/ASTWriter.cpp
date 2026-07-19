93b8251383bb1c9284f70d562fe464c2d1afb52c
Kevin Sala Penades <salapenades1@llnl.gov>
2026-07-19 00:13:57 -0700

    [Clang][OpenMP] Add parsing/sema for dims modifier in num_teams and thread_limit (#206412)
    Add parsing and sema support for `dims` modifier (OpenMP 6.1) in `num_teams` and
    `thread_limit` clauses. Example:
    
    ```cpp
    constexpr int N = 2;
    #pragma omp teams num_teams(dims(3): x, y, z) thread_limit(dims(N): a, b)
    { ... }
    ```

--- clang/lib/Serialization/ASTWriter.cpp
+++ clang/lib/Serialization/ASTWriter.cpp
@@ -8618,6 +8618,9 @@ void OMPClauseWriter::VisitOMPNumTeamsClause(OMPNumTeamsClause *C) {
 
 void OMPClauseWriter::VisitOMPThreadLimitClause(OMPThreadLimitClause *C) {
   Record.push_back(C->varlist_size());
+  Record.writeEnum(C->getModifier());
+  Record.AddSourceLocation(C->getModifierLoc());
+  Record.AddStmt(C->getModifierExpr());
   VisitOMPClauseWithPreInit(C);
   Record.AddSourceLocation(C->getLParenLoc());
   for (auto *VE : C->varlist())
